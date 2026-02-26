ab358a7f41451cd5a3dbf9116fa91c9c135f6074
sunflowerdahlia8 <sunflowerdahlia8@gmail.com>
2026-02-27 00:30:31 +0800

    Add files via upload

---
+++ DataTypes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace DataTypeApp
+{
+    class DataTypeProgram
+    {
+        static void Main (string[] args)
+        {
+            string fullName = "Juan Antonio Dela Cruz";
+            int age = 21;
+            double height = 5.7;
+            bool isStudent = true;
+            char grade = 'A';
+            const double Pi = 3.14;
+
+            Console.WriteLine("Full name: " + fullName);
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Height: " + height);
+            Console.WriteLine("Is he/she/they a student?: " + isStudent);
+            Console.WriteLine("What is his/her/their grade?: " + grade);
+            Console.WriteLine("Pi: " + Pi);
+
+            Console.WriteLine("\nPress any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GreetingApp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace ConsoleApp
+{
+    class GreetingProgram
+    {
+        static void Main (string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter the total number of your enrolled courses: ");
+            int enrolled;
+            enrolled = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the price of your favorite book: ");
+            double price = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Total enrolled courses: " + enrolled);
+            Console.WriteLine("Price of my favorite book: " + price);
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
