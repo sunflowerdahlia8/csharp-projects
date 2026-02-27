75cb10bf33588bb2ef01966695eafc4abd3e1627
Naiyana <sunflowerdahlia8@gmail.com>
2026-02-28 01:31:47 +0800

    Add files via upload

---
+++ bmiIndex.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MathApp
+{
+    class MathProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your weight in kilograms: ");
+            double weightKg = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter your height in centimeters: ");
+            double heightCm = Convert.ToDouble(Console.ReadLine());
+
+            weightKg = Math.Abs(weightKg);
+            heightCm = Math.Max(50, Math.Min(heightCm, 250));
+
+            double heightMi = heightCm / 100;
+            double bmi = weightKg / Math.Pow(heightMi, 2);
+            double myBmi = Math.Round(bmi, 2);
+
+            string category;
+            if (myBmi < 18.5)
+                category = "Underweight";
+            else if (myBmi < 25)
+                category = "Normal";
+            else if (myBmi < 30)
+                category = "Overweight";
+            else
+                category = "Obese";
+
+            Console.WriteLine();
+            Console.WriteLine("BMI RESULT");
+            Console.WriteLine("Your BMI: " + myBmi + " (" + category + ")");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ computerAverage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace ComputerAverage
+{
+    class ComputerAverageProgram
+    {
+        public static void Main (string[] args)
+        {
+            Console.WriteLine("Enter 5 grades separated by new line: ");
+
+            int first;
+            first = Convert.ToInt32(Console.ReadLine());
+            int second;
+            second = Convert.ToInt32(Console.ReadLine());
+            int third;
+            third = Convert.ToInt32(Console.ReadLine());
+            int fourth;
+            fourth = Convert.ToInt32(Console.ReadLine());
+            int fifth;
+            fifth = Convert.ToInt32(Console.ReadLine());
+
+            double totalGrades = first + second + third + fourth + fifth;
+            double average = totalGrades / 5;
+
+            int roundOff;
+            roundOff = Convert.ToInt32(Math.Round(average));
+
+            Console.WriteLine("The average is " +  average + " and round off to " + roundOff);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
