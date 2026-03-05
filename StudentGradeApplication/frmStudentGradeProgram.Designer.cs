206a266a917482d416f5c75a9989722f1051e731
Naiyana <sunflowerdahlia8@gmail.com>
2026-03-06 00:33:03 +0800

    Add files via upload

---
+++ StudentGradeApplication/frmStudentGradeProgram.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace StudentGradeApplication
+{
+    partial class frmStudentGradeProgram
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtEnglish = new System.Windows.Forms.TextBox();
+            this.txtMath = new System.Windows.Forms.TextBox();
+            this.txtScience = new System.Windows.Forms.TextBox();
+            this.txtFilipino = new System.Windows.Forms.TextBox();
+            this.txtHistory = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.lblPassed = new System.Windows.Forms.Label();
+            this.lblGrade = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(94, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Grades";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "English:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Math:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 170);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Science:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 211);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Filipino:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 250);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "History:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(74, 13);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(389, 26);
+            this.txtName.TabIndex = 7;
+            // 
+            // txtEnglish
+            // 
+            this.txtEnglish.Location = new System.Drawing.Point(84, 84);
+            this.txtEnglish.Name = "txtEnglish";
+            this.txtEnglish.Size = new System.Drawing.Size(84, 26);
+            this.txtEnglish.TabIndex = 8;
+            // 
+            // txtMath
+            // 
+            this.txtMath.Location = new System.Drawing.Point(84, 129);
+            this.txtMath.Name = "txtMath";
+            this.txtMath.Size = new System.Drawing.Size(84, 26);
+            this.txtMath.TabIndex = 9;
+            // 
+            // txtScience
+            // 
+            this.txtScience.Location = new System.Drawing.Point(84, 170);
+            this.txtScience.Name = "txtScience";
+            this.txtScience.Size = new System.Drawing.Size(84, 26);
+            this.txtScience.TabIndex = 10;
+            // 
+            // txtFilipino
+            // 
+            this.txtFilipino.Location = new System.Drawing.Point(84, 211);
+            this.txtFilipino.Name = "txtFilipino";
+            this.txtFilipino.Size = new System.Drawing.Size(84, 26);
+            this.txtFilipino.TabIndex = 11;
+            // 
+            // txtHistory
+            // 
+            this.txtHistory.Location = new System.Drawing.Point(84, 250);
+            this.txtHistory.Name = "txtHistory";
+            this.txtHistory.Size = new System.Drawing.Size(84, 26);
+            this.txtHistory.TabIndex = 12;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(16, 282);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(152, 75);
+            this.btnGenerate.TabIndex = 13;
+            this.btnGenerate.Text = "Generate Average";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // lblPassed
+            // 
+            this.lblPassed.AutoSize = true;
+            this.lblPassed.Location = new System.Drawing.Point(251, 170);
+            this.lblPassed.Name = "lblPassed";
+            this.lblPassed.Size = new System.Drawing.Size(0, 20);
+            this.lblPassed.TabIndex = 14;
+            // 
+            // lblGrade
+            // 
+            this.lblGrade.AutoSize = true;
+            this.lblGrade.Location = new System.Drawing.Point(197, 191);
+            this.lblGrade.Name = "lblGrade";
+            this.lblGrade.Size = new System.Drawing.Size(0, 20);
+            this.lblGrade.TabIndex = 15;
+            // 
+            // frmStudentGradeProgram
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(601, 398);
+            this.Controls.Add(this.lblGrade);
+            this.Controls.Add(this.lblPassed);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.txtHistory);
+            this.Controls.Add(this.txtFilipino);
+            this.Controls.Add(this.txtScience);
+            this.Controls.Add(this.txtMath);
+            this.Controls.Add(this.txtEnglish);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmStudentGradeProgram";
+            this.Text = "Student Grade Application";
+            this.Load += new System.EventHandler(this.frmStudentGradeProgram_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtEnglish;
+        private System.Windows.Forms.TextBox txtMath;
+        private System.Windows.Forms.TextBox txtScience;
+        private System.Windows.Forms.TextBox txtFilipino;
+        private System.Windows.Forms.TextBox txtHistory;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label lblPassed;
+        private System.Windows.Forms.Label lblGrade;
+    }
+}
+
---
+++ StudentGradeApplication/frmStudentGradeProgram.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentGradeApplication
+{
+    public partial class frmStudentGradeProgram : Form
+    {
+        public frmStudentGradeProgram()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string studentName = txtName.Text;
+                double english = Convert.ToDouble(txtEnglish.Text);
+                double math = Convert.ToDouble(txtMath.Text);
+                double science = Convert.ToDouble(txtScience.Text);
+                double filipino = Convert.ToDouble(txtFilipino.Text);
+                double history = Convert.ToDouble(txtHistory.Text);
+
+                double average = (english + math + science + filipino + history) / 5;
+
+                string passed = (average >= 75.00) ? "The student passed." : "The student failed.";
+
+                lblPassed.Text = passed;
+                lblGrade.Text = "The general average of " + studentName + " is " + average.ToString("0.00") + ".";
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Invalid input. Please enter numbers only.");
+            }
+        }
+
+        private void frmStudentGradeProgram_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
